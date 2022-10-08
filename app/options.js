01d4b95b76291511e02a20ca7e24d665c854bb35
KeyMeerkat <104410001+KeyMeerkat@users.noreply.github.com>
2022-10-08 15:17:10 +1100

    Change variable names

--- app/options.js
+++ app/options.js
@@ -1,25 +1,25 @@
-var popupBoxOptions = document.getElementById("popup-box-options");
-popupBoxOptions.style.display = ""; //("") [to show] and ("none") [to hide]
-popupBoxOptions.style.opacity = 0;
-popupBoxOptions.style.bottom = "100%";
+var hoverboxBoxOptions = document.getElementById("hoverbox-box-options");
+hoverboxBoxOptions.style.display = ""; //("") [to show] and ("none") [to hide]
+hoverboxBoxOptions.style.opacity = 0;
+hoverboxBoxOptions.style.bottom = "100%";
 
-function closeOptionsPopup() {
-  popupBoxOptions.style.opacity = 0;
-  popupBoxOptions.style.top = "100%";
+function closeOptionsHoverbox() {
+  hoverboxBoxOptions.style.opacity = 0;
+  hoverboxBoxOptions.style.top = "100%";
 }
 
-function openOptionsPopup() {
-  popupBoxOptions.style.opacity = 1;
-  popupBoxOptions.style.top = "50%";
+function openOptionsHoverbox() {
+  hoverboxBoxOptions.style.opacity = 1;
+  hoverboxBoxOptions.style.top = "50%";
 }
 
 function submitOptionsInfo() {
-  closeOptionsPopup();
+  closeOptionsHoverbox();
 }
 
 //Buttons
-var buttonOptionsCancel = document.getElementById("popup-options-cancel");
-var buttonOptionsSubmit = document.getElementById("popup-options-submit-info");
+var buttonOptionsCancel = document.getElementById("hoverbox-options-cancel");
+var buttonOptionsSubmit = document.getElementById("hoverbox-options-submit-info");
 
 //Variables
 
@@ -29,6 +29,6 @@ var buttonOptionsSubmit = document.getElementById("popup-options-submit-info");
 //Submit the info given and store on the browsers local storage
 function submitOptionsInfo() {
   
-  //Close the popup
-  closePopup();
+  //Close the hoverbox
+  closeHoverbox();
 };
